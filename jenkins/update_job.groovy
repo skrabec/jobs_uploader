932664d22fbf200a16ca19e97fd96c742571c42e
Nikolaj Skrabec <skrabets@gmail.com>
2025-02-18 23:08:50 +0100

    edit virtenv

--- jenkins/update_job.groovy
+++ jenkins/update_job.groovy
@@ -7,14 +7,15 @@ timeout(5) {
         stage("Install jenkins-jobs") {
             sh '''
                 apt-get update
-                apt-get install -y python3-pip
-                pip3 install jenkins-job-builder==6.4.2
+                apt-get install -y python3-venv
+                python3 -m venv /opt/jenkins_jobs_venv
+                /opt/jenkins_jobs_venv/bin/pip install jenkins-job-builder==6.4.2
             '''
         }
         
         stage("Deploy changes to jenkins") {
             dir("jenkins") {
-                sh "jenkins-jobs --conf ./jobs/jobs.ini update ./jobs"
+                sh "/opt/jenkins_jobs_venv/bin/jenkins-jobs --conf ./jobs/jobs.ini update ./jobs"
             }
         }
     }
